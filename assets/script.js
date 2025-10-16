62429b8508e95593456961b70fd91a1109dfc396
RubiBrums <rubibrums.oficial@gmail.com>
2025-10-16 18:42:28 -0300

    acs

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,7 @@
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
 
 const supabaseUrl = 'https://zofktotpjvmgikzmqpwd.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpvZmt0b3RwanZtZ2lrem1xcHdkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA0NjQzMDgsImV4cCI6MjA3NjA0MDMwOH0.ZnFAxmch40wWBda-zawJ9VZ_7ML7sKKYiiTbTUFZUdw' // tu clave pública (anon key)
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InpvZmt0b3RwanZtZ2lrem1xcHdkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjA0NjQzMDgsImV4cCI6MjA3NjA0MDMwOH0.ZnFAxmch40wWBda-zawJ9VZ_7ML7sKKYiiTbTUFZUdw'
 
 const supabase = createClient(supabaseUrl, supabaseKey)
 
@@ -35,14 +35,20 @@ async function mostrarDetalles(id) {
     detalleNombre.textContent = data.nombre;
     detalleDesc.textContent = data.descripcion;
 
-    const nombreImg = data.nombre.toLowerCase().replaceAll(' ', '');
+    const nombreImg = data.nombre
+        .toLowerCase()
+        .normalize('NFD')
+        .replace(/[\u0300-\u036f]/g, '')
+        .replaceAll(' ', '')
+        .replaceAll('-', '')
+        .replaceAll('_', '');
+
     detalleImg.src = `assets/img/full_${nombreImg}.png`;
     detalleImg.alt = data.nombre;
 }
 
 const enlacesFiltro = document.querySelectorAll('.nav-filter');
 const cartas = document.querySelectorAll('#contenedorCartas .card');
-
 
 enlacesFiltro.forEach(enlace => {
     enlace.addEventListener('click', (e) => {
